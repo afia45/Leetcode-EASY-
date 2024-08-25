c664eaf27e995cd912549a7a442bba533a83f3e4
afia45 <156591108+afia45@users.noreply.github.com>
2024-08-25 20:58:06 +0600

    Add files via upload

---
+++ Solutions/1. Two Sum/Brute force method.css
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //Brute Force Approach
+        //Time Complexity is O(n^2) for running two loops
+        int n, i, j;
+        n = nums.size();
+        for (i=0; i<(n-1); i++){ //first element
+            for (j=(i+1); j<n; j++){ //second element
+                if(nums[i]+nums[j]==target){
+                    return {i,j}; //return i and j
+                }
+            }
+        }
+
+        return {}; //else no solution
+    }
+};
